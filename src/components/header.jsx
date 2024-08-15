d61abb9fd2e910c1c69a8734e3e46cfc6427ffc1
Yusupov Javohir <javohiryyusupov6@gmail.com>
2024-08-15 11:48:40 +0500

    commit for navigate into [/]

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import RightDrawer from "./Drawer";
 import { useCurrencyContext } from "./CurrencyContext";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
   const { currency, setCurrency } = useCurrencyContext();
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate("/");
+  }
 
   return (
     <header className="bg-black text-white py-4 shadow-md">
       <div className="container mx-auto flex items-center justify-between max-w-3xl px-4">
         <div className="flex items-center">
           <span className="text-2xl font-bold cursor-pointer">
-            <span className="text-[#87CEEB] relative z-10">CRYPTOFOLIO</span>
+            <span onClick={handleClick} className="text-[#87CEEB] relative z-10">CRYPTOFOLIO</span>
           </span>
         </div>
         <nav className="flex items-center">
--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,20 +1,20 @@
 
-import { Pagination } from "flowbite-react";
-import { useState } from "react";
+// import { Pagination } from "flowbite-react";
+// import { useState } from "react";
 
- function Pagination() {
-   const [currentPage, setCurrentPage] = useState(1);
+//  function Pagination() {
+//    const [currentPage, setCurrentPage] = useState(1);
 
-   const onPageChange = (page) => setCurrentPage(page);
+//    const onPageChange = (page) => setCurrentPage(page);
 
-   return (
-     <div className="flex overflow-x-auto sm:justify-center">
-       <Pagination
-         currentPage={currentPage}
-         totalPages={100}
-         onPageChange={onPageChange}
-       />
-     </div>
-   );
- }
- export default Pagination;
+//    return (
+//      <div className="flex overflow-x-auto sm:justify-center">
+//        <Pagination
+//          currentPage={currentPage}
+//          totalPages={100}
+//          onPageChange={onPageChange}
+//        />
+//      </div>
+//    );
+//  }
+//  export default Pagination;
